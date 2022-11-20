130911833e361ae12603b2f94d404a0263316dc2
Nazar373 <99971906+Nazar373@users.noreply.github.com>
2022-11-20 18:34:42 +0000

    Deploying to gh-pages from @ Nazar373/goit-react-hw-06-phonebook@083d69bb88150400b87d257b3d7117231bb8dce9 🚀

---
+++ precache-manifest.fb156d9aa267a0d355dc6a4f3e831b87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50ea939dba5ad5e9f3e5",
+    "url": "./static/css/main.e398aa84.chunk.css"
+  },
+  {
+    "revision": "50ea939dba5ad5e9f3e5",
+    "url": "./static/js/main.5c49237a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d80fd82c2c3394ef66ca",
+    "url": "./static/js/2.92a86d4c.chunk.js"
+  },
+  {
+    "revision": "c8f16964b4c5f587e675a78945c69e1f",
+    "url": "./index.html"
+  }
+];
